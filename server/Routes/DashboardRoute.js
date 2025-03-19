e380b08b59a92a1c3a3884e4d2a63c6eaad44263
SHERVIN DANI R V <114435466+ShervinDani@users.noreply.github.com>
2025-03-19 14:18:43 +0000

    Change

--- server/Routes/DashboardRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const {userRegistration, petTypes} = require('../Controller/Dashboard')
-
-router.get('/user-registrations', userRegistration)
-router.get('/pet-types', petTypes)
-
-module.exports = router
--- server/Routes/OtpRoute.js
+++
@@ -1,9 +0,0 @@
-const { genOtp, verifyOtp, forgotOtp } = require('../Controller/OtpController');
-const express = require('express');
-const router = express.Router();
-
-router.post('/genotp', genOtp);
-router.post('/verifyotp', verifyOtp);
-router.post('/forgototp', forgotOtp);
-
-module.exports = router;
